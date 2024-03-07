18e7475c69245d5589fd3918d4a3ae2cb0d7ca5e
bidoof <bluetoothdoof@proton.me>
2024-03-07 10:38:33 -0300

    added dropdown on rules

---
+++ new/code/rules-drop.js
@@ -0,0 +1,11 @@
+function selectRulesTab(name) {
+    tabs = ['test', 'stat', 'condition', 'hitch', 'potential', 'skill', 'combat', 'creation', 'life', 'item'];
+
+    for (const tab of tabs) {
+        if (tab === name) {
+            document.getElementById(tab).classList.remove('d-none');
+        } else {
+            document.getElementById(tab).classList.add('d-none');
+        }
+    }
+}
